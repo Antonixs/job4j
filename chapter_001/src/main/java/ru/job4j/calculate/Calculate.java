f549f492420def3cbef27d0e95d82dd46da9d095
Antonixs <antonixs2017@mail.ru>
2018-08-22 14:31:53 +0500

    1.3. JavDoc. Code convetion

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,18 @@
 package ru.job4j.calculate; 
 
+/**
+    *Класс выводит на экран надпись Привет мир!
+	*@author antonshmakov (antonixs2017@mail.ru)
+    *@since 22.08.2018
+    *@version 1
+    *
+*/
  public class Calculate {
 
+ /**
+	*@param args - args.
+	*@return Echo plus args
+	*/
         public static void main(String[] args)  {
 			System.out.println("Hello World"); 
 		} 
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,17 @@
+/**
+* Package for calculate task.
+*
+* @author Anton Shmakov (mailto:antonixs2017@mail.ru)
+* @version 1
+* @since 22.08.2018
+*/
+package ru.job4j.calculate; 
+
+ public class Calculate {
+	 
+        public static void main(String[] args)  {
+			System.out.println("Hello World"); 
+		} 
+		
+		}
+	
