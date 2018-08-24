c056a881e442851833fe4bcec04c577d2fa4fefb
Antonixs <antonixs2017@mail.ru>
2018-08-24 13:30:32 +0500

    mmm

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -11,11 +11,18 @@ package ru.job4j.calculate;
 
  /**
 	*@param args - args.
-	*@return Echo plus args
 	*/
         public static void main(String[] args)  {
 			System.out.println("Hello World"); 
 		} 
 		
+		/**
+     * Method echo.
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 		}
 	
